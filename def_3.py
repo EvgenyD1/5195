3d3c434c4d58b928e1e8d5c56fc0af0d434c1402
Evgeniy <vsidm@mail.ru>
2024-05-07 21:00:48 +0300

    first commit

---
+++ def_3.py
@@ -0,0 +1,31 @@
+# Дан список поисковых запросов. Получить распределение количества слов в них. 
+# Т.е. поисковых запросов из одного - слова 5%, из двух - 7%, из трех - 3% и т.д.
+
+queries = [
+    'смотреть сериалы онлайн',
+    'новости спорта',
+    'афиша кино',
+    'курс доллара',
+    'сериалы этим летом',
+    'курс по питону',
+    'сериалы про спорт'
+    ]
+
+
+def requests_distributed(_list):
+    dict_ = {}
+    list_ = []
+    list_1 = []
+    for item in _list:
+        result = item.split()
+        dict_.update({len(result):result})
+        list_.append(dict_)
+    for my_dict in list_:
+        for key, value in my_dict.items():
+            percentage = round((len(value) / len(queries)) * 100, 2)
+            list_1.append(f'Поисковых запросов из {key} слов: {percentage}% ')
+        return list_1
+
+
+if __name__ == '__main__':
+    requests_distributed(queries)
